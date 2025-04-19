555b54ee077166a3c1a9d0a88814d9f3324e11b1
nltran15@gmail.com <nltran15@gmail.com>
2025-04-19 15:37:09 +0000

    fix : remove useless sources

--- models/staging/stg_local_bike__products.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    product_id,
-    product_name, 
-    brand_id AS product_brand_id, 
-    category_id AS product_category_id, 
-    model_year AS product_model_year,
-    list_price AS product_price
-FROM {{ source('local_bike', 'products') }}
