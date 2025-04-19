a4c0c18dbea0388fa2ebeb3a91ff7da395e84bfe
nltran15@gmail.com <nltran15@gmail.com>
2025-04-19 15:28:26 +0000

    add : add last mart to be connected to a dashboard

---
+++ models/mart/mrt_local_bike__daily_report.sql
@@ -0,0 +1,18 @@
+{{ config(materialized='table') }}
+
+SELECT 
+    order_date,
+    EXTRACT(MONTH FROM order_date) as order_month,
+    EXTRACT(YEAR FROM order_date) as order_year,
+    store_name,
+    COUNT(*) AS count_orders,
+    COUNT(DISTINCT customer_id) AS count_distinct_customers,
+    ROUND(SUM(total_order_amount),2) AS sum_total_amount,
+    SUM(total_items_by_order) AS sum_total_items_by_orders
+FROM {{ ref('int_local_bike__orders') }}
+GROUP BY 
+    order_date,
+    EXTRACT(MONTH FROM order_date),
+    EXTRACT(YEAR FROM order_date),
+    store_name
+    
